ac3f4846c043630d055b6e4adc8872edd14ba1be
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2023-01-18 19:21:47 +0100

    .

--- API/session_token/createToken.php
+++ API/session_token/createToken.php
@@ -1,9 +1,6 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: PUT");
-    header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
     include_once dirname(__FILE__) . '/../../COMMON/connect.php';
     include_once dirname(__FILE__) . '/../../MODEL/sessionToken.php';
