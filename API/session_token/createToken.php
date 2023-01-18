7424b7361cbea1668902503366bf073025cb68a9
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2023-01-18 19:43:26 +0100

    .

--- API/session_token/createToken.php
+++ API/session_token/createToken.php
@@ -1,23 +1,23 @@
 <?php
     require("../../COMMON/connect.php");
-    require("../../MODEL/user.php");
+    require("../../MODEL/sessionToken.php");
 
     header("Content-type: application/json; charset=UTF-8");
     header('Access-Control-Allow-Origin: *');
 
     $data = json_decode(file_get_contents("php://input"));
 
-    if (empty($data->email) || empty($data->password)) {
+    if (empty($data->token) || empty($data->user)) {
         http_response_code(400);
-        echo json_encode(["message" => "Fill every field"]);
+        echo json_encode(["message" => "Bad request"]);
         die();
     }
 
     $db = new Database();
     $db_conn = $db->connect();
-    $user = new User($db_conn);
+    $sessionToken = new SessionToken($db_conn);
 
-    $result = $user->login($data->email, $data->password);
+    $result = $sessionToken->createToken($data->user, $data->token);
 
     if ($result != false) {
         http_response_code(200);
