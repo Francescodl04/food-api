bf8900801f2d7deebaec5b488469b62ac7169146
Francescodl04 <72445602+Francescodl04@users.noreply.github.com>
2023-01-21 15:21:24 +0100

    Update updateToCompleted.php

--- API/order/updateToCompleted.php
+++ API/order/updateToCompleted.php
@@ -7,11 +7,7 @@
  * A.S. 2022-2023
 **/
 
-header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: PUT");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once dirname(__FILE__) . '/../../COMMON/connect.php';
 include_once dirname(__FILE__) . '/../../MODEL/order.php';
